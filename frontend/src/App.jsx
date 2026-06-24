01989c8eb11b8304ef38578429354affa00949b4
mimansaknepal <74827557+Arenoid@users.noreply.github.com>
2026-06-24 20:22:44 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const handleTrack = async(e) =>{
 
 try {
   const response = await fetch(
-    `http://127.0.0.1:8080/api/track?lat=${lat}&lon=${lon}&sat=${satId}`
+    `https://satfication.onrender.com/api/track?lat=${lat}&lon=${lon}&sat=${satId}`
   );
   const data = await response.json()
 
